f5037f68a0ca5133a42bf1f9c1d2380cbdf965cb
somayejafari <168428987+somayejafari@users.noreply.github.com>
2024-05-25 11:15:50 +0330

    Chat-Project-react

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+ app.use(cors());
+
+ const server = http.createServer(app);
+const {Server} = require("socket.io");
+const { log } = require("console");
+
+const io = new Server(server, {
+    cors: {
+        origin : "http://localhost:3000",
+        methods : [ "GET", "POST"] 
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`userconnected: ${socket.id}`);
+
+    socket.on("join-room", (data) => {
+        socket.join(data)
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    })
+    socket.on("send_message", (data) => {
+       socket.to(data.room).emit("receive_message", data);
+    })
+    socket.on("disconnect", () => {
+        console.log("user disconnected",socket.id);
+    })
+});
+ server.listen(3001, () => {
+    console.log("SERVER RUNING");
+ });
